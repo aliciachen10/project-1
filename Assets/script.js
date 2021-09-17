f027f3dd8312ec4180a4ea098f3b06b477919b67
cam <cam@camherbert.com>
2021-09-16 20:25:31 -0400

    Added API calls for everything as proof of concepts - need to make into functions

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,6 @@
 var returnData;
 
+// concept for getting search results from Alphavantage API
 fetch('https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=amaz&apikey=iuygasod78g')
   .then(function (response) {
       return response.json();
@@ -9,9 +10,10 @@ fetch('https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=amaz&ap
       for (var i = 0; i < data['bestMatches'].length; i++) {
           searchResults.push(data['bestMatches'][i]);
       }
-      console.log(searchResults);
+      // console.log(searchResults);
   });
 
+// concept for getting the historical data from a stock
 fetch('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo')
   .then(function (response) {
     return response.json();
@@ -30,6 +32,41 @@ fetch('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&a
     }
     // console.log(dataset);
   });
+
+// concept for using FinnHub API results to search for crypto and outputting results 
+fetch('https://finnhub.io/api/v1/crypto/symbol?exchange=coinbase&token=c50jesqad3ic9bdl9ojg')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    var filteredResults = [];
+    for (var entry in data) {
+      // console.log(data[entry]['displaySymbol']);
+      if (data[entry]['displaySymbol'].includes('BTC')) {
+        filteredResults.push(data[entry]);
+      }
+    }
+    console.log(filteredResults);
+  });
+
+// concept for getting current stock data results from the AlphaVantage API
+fetch('https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=IBM&apikey=demo')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
+
+// concept for getting current crypto info results from the AlphaVantage API
+fetch('https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=CNY&apikey=demo')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  })
+
 
 // crypto exchanges:
 // ["BITTREX","BITFINEX","BITMEX","HUOBI","FXPIG","GEMINI","COINBASE","KRAKEN","HITBTC","BINANCE","KUCOIN","OKEX","ZB","POLONIEX"]
